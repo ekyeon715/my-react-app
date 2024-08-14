edff9c96dca42f0bc2501bdaa2385816444d82ac
ekyeon715 <rkdwldus9636@naver.com>
2024-08-14 17:10:29 +0900

    feat: 국가추가 유효성 검사 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  
   const [country, setCountry] = useState("");
   const [gold, setGold] = useState(0);
   const [silver, setSilver] = useState(0);
   const [bronze, setBronze] = useState(0);
   const [countries, setCountries] = useState([]);
-  const addCountryHandler = () => {
-    const newCountry = {
-      id: new Date().getTime(),
-      country: country,
-      gold: gold,
-      silver: silver,
-      bronze: bronze
-    };
 
-    setCountries([...countries, newCountry].sort((a, b) => b.gold - a.gold));
+  const checkExistance = () => {
+    return countries.find((c) => {
+      if (c.country.toLowerCase() === country.toLowerCase()) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  const addCountryHandler = (e) => {
+    e.preventDefault();
+    if (checkExistance()) {
+      alert("이미 등록된 국가입니다");
+    } else {
+      const newCountry = {
+        id: new Date().getTime(),
+        country: country,
+        gold: gold,
+        silver: silver,
+        bronze: bronze,
+      };
+      setCountries([...countries, newCountry].sort((a, b) => b.gold - a.gold));
+    }
   };
 
   const updateCountryHandler = () => {
@@ -27,8 +41,8 @@ const App = () => {
       country: country,
       gold: gold,
       silver: silver,
-      bronze: bronze
-    }
+      bronze: bronze,
+    };
     const result = countries.map(function (item) {
       if (item.country === country) {
         return updatedCountry;
@@ -41,52 +55,76 @@ const App = () => {
   // (2) 일치 할 경우에는 해당 객체의 골드,실버,브론즈의 밸류를 입력한 값으로 바꿔서 리턴
   // (3) 일치하지 않을 경우 그대로 리턴
 
-
   const deleteCountryHandler = (id) => {
     const deletedCountry = countries.filter(function (country) {
       return country.id != id;
     });
-    
-    setCountries(deletedCountry);
-  }
 
+    setCountries(deletedCountry);
+  };
 
   return (
-    <main className='container'>
-      <h3 className='title'>2024 파리올림픽</h3>
-      <div className='inner-box'>
-        <div className='inputBox'>
+    <main className="container">
+      <h3 className="title">2024 파리올림픽</h3>
+      <div className="inner-box">
+        <div className="inputBox">
           <p>국가명</p>
-          <input type="text" placeholder='국가 입력'
-            value={country} onChange={(e) => {
+          <input
+            type="text"
+            placeholder="국가 입력"
+            value={country}
+            onChange={(e) => {
               setCountry(e.target.value);
-            }} />
+            }}
+          />
         </div>
-        <div className='inputBox'>
+        <div className="inputBox">
           <p>금메달</p>
-          <input type="number" value={gold} onChange={(e) => {
-            setGold(e.target.value);
-          }} />
+          <input
+            type="number"
+            value={gold}
+            onChange={(e) => {
+              setGold(e.target.value);
+            }}
+          />
         </div>
-        <div className='inputBox'>
+        <div className="inputBox">
           <p>은메달</p>
-          <input type="number" value={silver} onChange={(e) => {
-            setSilver(e.target.value);
-          }} />
+          <input
+            type="number"
+            value={silver}
+            onChange={(e) => {
+              setSilver(e.target.value);
+            }}
+          />
         </div>
-        <div className='inputBox'>
+        <div className="inputBox">
           <p>동메달</p>
-          <input type="number" value={bronze} onChange={(e) => {
-            setBronze(e.target.value);
-          }} />
+          <input
+            type="number"
+            value={bronze}
+            onChange={(e) => {
+              setBronze(e.target.value);
+            }}
+          />
         </div>
-        <button className='button' onClick={addCountryHandler}>국가추가</button>
-        <button className='button' onClick={updateCountryHandler}>업데이트</button>
+        <button className="button" onClick={addCountryHandler}>
+          국가추가
+        </button>
+        <button className="button" onClick={updateCountryHandler}>
+          업데이트
+        </button>
       </div>
 
       <table border="0">
         <thead>
-          <tr><th>국가별</th><th>금메달</th><th>은메달</th><th>동메달</th><th>액션</th></tr>
+          <tr>
+            <th>국가별</th>
+            <th>금메달</th>
+            <th>은메달</th>
+            <th>동메달</th>
+            <th>액션</th>
+          </tr>
         </thead>
         <tbody>
           {countries.map((c) => (
@@ -95,15 +133,17 @@ const App = () => {
               <td>{c.gold}</td>
               <td>{c.silver}</td>
               <td>{c.bronze}</td>
-              <td><button onClick={e => deleteCountryHandler(c.id)}>삭제</button></td>
+              <td>
+                <button onClick={(e) => deleteCountryHandler(c.id)}>
+                  삭제
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     </main>
   );
-}
+};
 
 export default App;
-
-
